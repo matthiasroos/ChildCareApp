6cada5e4cc77230e25a1ed33bb895945aa7650ac
Matthias Roos <matthias.roos@freenet.de>
2022-09-10 20:22:12 +0200

    Add unit test for authenticate() in BasicAuthBackend

---
+++ backend/app/REST/fastapi/tests/test_server.py
@@ -0,0 +1,24 @@
+import asyncio
+
+import fastapi.requests
+import starlette.datastructures
+
+import backend.app.REST.fastapi.server
+
+
+def test_authenticate_all_is_working():
+    headers = starlette.datastructures.Headers(
+        {'Authorization': 'Basic dGVzdF91c2VyOkRnaWRYcXB0OVJJNGM3WDN0MkYz'})
+    scope = {
+        'method': 'GET',
+        'type': 'http',
+        'headers': None}
+    conn = fastapi.requests.HTTPConnection(scope=scope)
+    conn._headers = headers
+
+    basicauthbackend = backend.app.REST.fastapi.server.BasicAuthBackend(role='admin')
+
+    credentials, user = asyncio.run(basicauthbackend.authenticate(conn=conn))
+
+    assert credentials
+    assert user
