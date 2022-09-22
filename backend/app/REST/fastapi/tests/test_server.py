40c28a9991f0f908bb8ada1bbdfc6b78270814dd
Matthias Roos <matthias.roos@freenet.de>
2022-09-22 20:40:30 +0200

    Add unit test for CloneRequestMiddleware

--- backend/app/REST/fastapi/tests/test_server.py
+++ backend/app/REST/fastapi/tests/test_server.py
@@ -1,6 +1,10 @@
 import asyncio
+import unittest.mock
 
 import fastapi.requests
+import fastapi.testclient
+import pydantic
+import pytest
 import starlette.datastructures
 
 import backend.app.REST.fastapi.server
@@ -22,3 +26,53 @@ def test_authenticate_all_is_working():
 
     assert credentials
     assert user
+
+
+@pytest.fixture
+def testclient():
+    app = fastapi.FastAPI()
+    app.add_middleware(backend.app.REST.fastapi.server.CloneRequestMiddleware,
+                       servers=['test_url:9000'])
+
+    class InputModel(pydantic.BaseModel):
+        int_param: int
+        str_param: str
+
+    @app.get('/test/{path_param_1}')
+    def get_call(path_param_1: str, body_input: InputModel, skip: int = 0, limit: int = 10):
+        pass
+
+    client = fastapi.testclient.TestClient(app)
+    return client
+
+
+def test_get_call(testclient):
+    with unittest.mock.patch('requests.request') as mock_requests:
+
+        testclient.get('/test/abc',
+                       params={'skip': 0, 'limit': 10})
+
+    assert mock_requests.call_args.kwargs['url'] == 'http://test_url:9000/test/abc'
+    assert mock_requests.call_args.kwargs['params'] == b'skip=0&limit=10'
+
+
+def test_post_call_dict_as_json(testclient):
+    with unittest.mock.patch('requests.request') as mock_requests:
+
+        testclient.post('/test/abc',
+                        params={'skip': 0, 'limit': 10},
+                        json={'int_param': 7, 'str_param': 'teststr'})
+
+    assert mock_requests.call_args.kwargs['params'] == b'skip=0&limit=10'
+    assert mock_requests.call_args.kwargs['data'] == b'{"int_param": 7, "str_param": "teststr"}'
+
+
+def test_post_call_bytes_as_data(testclient):
+    with unittest.mock.patch('requests.request') as mock_requests:
+
+        testclient.post('/test/abc',
+                        params={'skip': 0, 'limit': 10},
+                        data=b'{"int_param": 7, "str_param": "teststr"}')
+
+    assert mock_requests.call_args.kwargs['params'] == b'skip=0&limit=10'
+    assert mock_requests.call_args.kwargs['data'] == b'{"int_param": 7, "str_param": "teststr"}'
